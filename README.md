ab89f0a1c5538d4118707fb35f2cbc2a6b6eac20
Erik Palen <84192057+ekipalen@users.noreply.github.com>
2023-12-28 16:57:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Example: Python - Complete "The Automation Challenge"
+
+This example completes [The Automation Challenge](https://www.theautomationchallenge.com/). 
+
+This example leverages the new python open-source structure [robo](https://github.com/robocorp/robo), the [libraries](https://github.com/robocorp/robo#libraries) from to same project as well.
+
+Running the automation in the VSCode: 
+1. Install "Robocorp Code" and "Robotframework Language Server" VSCode extensions. 
+2. Download the code and open the folder in VSCode. 
+3. Update your username and password to the variables in the "tasks.py" file. 
+4. Click "Run" from the Robocorp Extension menu or type and select "Robocorp: Run Robot" in the Command Paletter (ctrl+shift+p).
+
+👉 After running the bot, check out the `log.html` under the `output` -folder.
+
+For more information, do not forget to checkout the following:
+* [Robocorp Documentation -site](https://robocorp.com/docs)
+* [Portal for more examples](https://robocorp.com/portal)
+* [robo repo](https://github.com/robocorp/robo) as this will developed a lot...
---
+++ tasks.py
@@ -0,0 +1,59 @@
+import time
+import os
+from robocorp import browser
+from robocorp.tasks import task
+from RPA.Excel.Files import Files as Excel
+
+# Set your username and password. Robocorp Vault is recommended 
+# Set the HEADLESS argument to True for faster execution without the visible browser. 
+USERNAME = "first.last@example.com"
+PASSWORD = "MyPassword"
+HEADLESS = False 
+
+@task
+def solve_challenge():
+    """Solve the automation challenge"""
+    browser.configure(
+        browser_engine="chrome",
+        screenshot="only-on-failure",
+        headless=HEADLESS,
+    )
+
+    page = browser.goto("https://www.theautomationchallenge.com/")
+    page.get_by_role("button", name="SIGN UP OR LOGIN").click()
+    page.get_by_role("button", name="OR LOGIN", exact=True).click()
+    page.get_by_role("textbox", name="Email").fill(USERNAME)
+    page.get_by_role("textbox", name="Password").fill(PASSWORD)
+    page.get_by_role("button", name="LOG IN").click()
+
+    with page.expect_download() as download_info:
+        page.get_by_text("Download Excel Spreadsheet").click()
+    download = download_info.value
+    filepath = os.path.join(os.getenv("ROBOT_ROOT"), download.suggested_filename)
+    download.save_as(filepath)
+
+    excel = Excel()
+    excel.open_workbook(filepath)
+    rows = excel.read_worksheet("data", header=True)
+    excel.close_workbook()
+
+    field_names = [["Company Name", "company_name"], ["Address", "company_address"], ["EIN", "employer_identification_number"], ["Sector", "sector"], ["Automation Tool", "automation_tool"], ["Annual Saving", "annual_automation_saving"], ["Date", "date_of_first_project"]]
+    page.get_by_role("button", name="Start").click()
+
+    for row in rows:
+        # remove possible capthca elements from the DOM :)
+        page.add_script_tag(content="document.querySelector('.CustomElement').remove();")
+        page.add_script_tag(content="document.querySelector('.greyout').remove();")
+        page.add_script_tag(content="document.querySelector('.Popup').remove();")
+
+        for field in field_names:
+             text_content, spreadsheet_header = field
+             input_selector = f'//div[@class="bubble-element Group"][.="{text_content}"][not(ancestor-or-self::*[contains(@style,"display: none;")])]//input'
+             element = page.query_selector(input_selector)
+             element.fill(row[spreadsheet_header])
+       
+        page.get_by_role("button", name="Submit").click()
+
+    os.remove(filepath)
+    time.sleep(5)
+    browser.screenshot()
